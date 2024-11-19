40ffeb777f066cc68c2d4aac7009bbfc83d2ec91
Emmanuel Ovares <emanuelovares@gmail.com>
2024-11-19 13:29:38 -0600

    Testing node

---
+++ index.js
@@ -0,0 +1,7 @@
+// Global variable than contains fetch, console, setTimeout, setInterval, etc
+// console.log(globalThis)
+
+// Common JS import syntax
+const { sum }= require('./sum')
+
+console.log(sum(1, 2))
